cca91f7f44051ef3e08be417b93a669d1bc3ddcc
Jair-2005 <steven.granizo@udla.edu.ec>
2026-05-13 15:03:05 -0500

    tercer commit

--- Punteros.c
+++ Punteros.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int vec[5]=(2,4,6,8,10);
+    int vec[5]={{2,4,6,8,10}};
     int *ptr;
     ptr=&vec[0];
     // printf("%d", ptr);
--- vocales.c
+++ vocales.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 int main(){
     char vocales[5]={'a','e','i','o','u'};
     char *ptr;
-    ptr=&vocales[0];
+    ptr=&vocales[4];
+    *ptr='x';
     for(int i=0;i<5;i++){
         printf("vocales[%d] %c\n ",i,vocales[i]);
         printf("%c\n ",*(ptr++));
