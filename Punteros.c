9f2f041bff6875e8749979e96fa3ac45e6e739ec
Jair-2005 <steven.granizo@udla.edu.ec>
2026-05-13 14:21:46 -0500

    segundo commit

--- Punteros.c
+++ Punteros.c
@@ -4,10 +4,10 @@ int main(){
     int vec[5]=(2,4,6,8,10);
     int *ptr;
     ptr=&vec[0];
-    printf("%d", ptr);
+    // printf("%d", ptr);
     for(int i=0;i<5;i++){
         printf("%d ",vec[i]);
-        printf("%d\n ",*ptr);
+        printf("%d\n ",*ptr++i);
     }
     return 0;
 }
